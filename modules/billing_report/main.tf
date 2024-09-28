535da08bae976056ccee749ebc123c342df4cb7e
kiraum <tfgoncalves@xpto.it>
2024-09-28 12:46:01 +0200

    fix(versions): adding providers versions

--- modules/billing_report/main.tf
+++ modules/billing_report/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
 # Get current AWS account information
 data "aws_caller_identity" "current" {}
 
--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
 resource "aws_route53_zone" "zones" {
   for_each = var.hosted_zones
 
