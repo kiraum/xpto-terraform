cad574cb6e30557b26a86769ab87244ddc68726f
kiraum <tfgoncalves@xpto.it>
2024-10-03 21:16:02 +0200

    chore(static_page): adding docstring to disable_cloudfront lambda

--- modules/static_website/disable_cloudfront.py
+++ modules/static_website/disable_cloudfront.py
@@ -1,3 +1,9 @@
+"""
+CloudFront Distribution Disabler for AWS Lambda.
+Disables a CloudFront distribution in response to a budget alert.
+Uses boto3 to interact with AWS services and sends notifications via SNS.
+"""
+
 import json
 import os
 
@@ -5,6 +11,30 @@ import boto3
 
 
 def lambda_handler(event, context):
+    """
+    AWS Lambda function to disable a CloudFront distribution and send notifications.
+
+    This function disables a specified CloudFront distribution in response to a budget alert.
+    It then sends an SNS notification about the action taken or any errors encountered.
+
+    Args:
+        event (dict): The event dict that contains the parameters passed when the function
+                      is invoked.
+        context (object): The context in which the function is called.
+
+    Returns:
+        dict: A dictionary containing a statusCode and a JSON-formatted body message.
+              - statusCode 200 and success message if the distribution is disabled successfully.
+              - statusCode 500 and error message if an exception occurs.
+
+    Environment Variables:
+        DISTRIBUTION_ID (str): The ID of the CloudFront distribution to be disabled.
+        SNS_TOPIC_ARN (str): The ARN of the SNS topic to publish notifications.
+
+    Raises:
+        Exception: Any exception that occurs during the execution of the function
+                   will be caught, logged, and notified via SNS.
+    """
     cloudfront = boto3.client("cloudfront")
     sns = boto3.client("sns")
     distribution_id = os.environ["DISTRIBUTION_ID"]
@@ -33,8 +63,8 @@ def lambda_handler(event, context):
         )
 
         return {"statusCode": 200, "body": json.dumps(message)}
-    except Exception as e:
-        error_message = f"Error disabling CloudFront distribution: {str(e)}"
+    except Exception as exc:
+        error_message = f"Error disabling CloudFront distribution: {str(exc)}"
         print(error_message)
 
         # Send SNS notification for error
